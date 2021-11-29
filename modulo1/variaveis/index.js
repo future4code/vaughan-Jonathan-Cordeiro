c7881a699dcab0eb28ff785ebed59598c3009da3
jhowramos <ramosjonatan7@hotmail.com>
2021-11-29 14:42:28 -0300

    exercicios de javascript

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,56 @@
+/* Exercicio 1
+
+let nome
+let idade
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+Apareceu uma mensagem escrita undefined, isso aparece quando nós não damos valor a uma variavel.
+
+let nomeDoUsuario = prompt ("Qual seu nome: ");
+let idadeDoUsuario = prompt ("Qual sua idade?: ");
+
+console.log(typeof nomeDoUsuario);
+console.log(typeof idadeDoUsuario);
+
+As variaveis que apareceram foram do tipo string, por que a mesma foi declarada com o valor de "" e isso a considera string
+
+console.log ("Olá, ", nomeDoUsuario ,"Você tem ", idadeDoUsuario, "anos.");
+*/
+
+/*Exercicio 2
+let pergunta1 = prompt ("Você gosta de jogos?");
+let pergunta2 = prompt ("Você gosta de café?");
+let pergunta3 = prompt ("Você gosta de filmes de comédia?");
+
+console.log("Você gosta de jogos? -", pergunta1)
+console.log("Você gosta de café? -", pergunta2)
+console.log("Você gosta de filmes de comédia? -", pergunta3)
+*/
+
+/* Exercicio 3
+
+let a = 10
+let b = 25
+
+c = a
+a = b
+b = c
+
+console.log("O novo valor de a é", a)
+console.log("O novo valor de b é", b)
+*/
+
+// Desafio
+
+let numero1 = prompt ("Digite um numero:")
+let numero2 = prompt ("Digite o segundo numero: ")
+
+let numeroEmNumber = Number(numero1)
+let numeroEmNumber2 = Number(numero2)
+
+console.log("O Primeiro numero somado ao segundo numero resulta em: ", numeroEmNumber + numeroEmNumber2);
+console.log("O Primeiro numero multiplicado pelo segundo número resulta em: ", numeroEmNumber * numeroEmNumber2);
+
+
