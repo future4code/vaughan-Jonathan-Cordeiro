33ce44b5c28928bd39cb32230e9ec6b70af42c67
jhowramos <ramosjonatan7@hotmail.com>
2021-12-08 17:24:22 -0300

    Aula lacos

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,72 @@
+// Exercicios de interpretação de Código
+
+//1) Ele ira repetir o código enquanto o valor for menor que 5. 
+//o resultado é 10 por que ele está somando o numero a cada repetição.
+
+//2)A) Ira imprimir no console todos os numeros que forem maior que 18.
+//B) if(maiorNumero >18 arrayDeNumeros[i])
+
+//3) iria imprir a cada linha um asterisco somado por ele, ex abaixo:
+/* 
+
+*
+**
+***
+****
+*/
+
+// 1. Pergunte ao usuário quantos bichinhos de estimação ele tem e guarde esse dado em uma variável. 
+
+// const quantidadeBichosEstimacaoUsuarioTem = Number(prompt("Quantos bixos você tem? "))
+// let listaDosAnimais = []
+// let nomeDosAnimais
+
+// if (quantidadeBichosEstimacaoUsuarioTem<=0){
+//     console.log("Que pena! Você pode adotar um pet!")
+// }else if (quantidadeBichosEstimacaoUsuarioTem>0){
+//     for(let nomeDosBixos =0; nomeDosBixos < quantidadeBichosEstimacaoUsuarioTem; nomeDosBixos++){
+//         nomeDosAnimais = prompt("Qual o nome dos animais?")
+//         listaDosAnimais.push(nomeDosBixos)
+
+//     }
+//console.log("O nome dos animais são:", listaDosAnimais)
+
+// }
+
+//2) Considere que você tenha acesso a um array  (chamado de 'array original') que é composto somente de números.
+//  Baseando-se nisso, crie uma função para cada um dos itens abaixo, realizando as operações pedidas:
+
+//const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+
+// a) Escreva um programa que **imprime** cada um dos valores do array original.
+
+// function arraysOriginais (){
+//     for(let i = 0; i < 12; i++){
+//         const numeros = array[i]
+//         console.log(numeros)
+//     }
+// }
+
+// console.log("Exercicio A:")
+// arraysOriginais()
+
+// b) Escreva um programa que **imprime** cada um dos valores do array original divididos por 10
+
+// function arraysDivididosPor10 (){
+//     for(let i = 0; i < 12; i++){
+//         const numeros = array[i] / 10
+//         console.log(numeros)
+//     }
+// }
+
+// console.log("Exercicio B") 
+// arraysDivididosPor10()
+
+
+// c) Escreva um programa que **crie** um novo array contendo, somente, os números pares do array original e 
+// **imprima** esse novo array
+
+
+// d) Escreva um programa que **crie** um novo array contendo strings, da seguinte forma: "O elemento do índex `i` é: `numero`". Depois, **imprima** este novo array.
+
+// e) Escreva um programa que imprima no console o maior e o menor números contidos no array original
