754b1c21634ecf442193a4b8823fba5b4a832aa7
jhowramos <ramosjonatan7@hotmail.com>
2022-01-04 13:14:18 -0300

    exercicio do 1 ao 6 finalizados

---
+++ modulo1/reforco-js-lista2/exercicios.js
@@ -0,0 +1,106 @@
+// ATENÇÃO!!!
+//    -> NÃO COMENTE NENHUMA DAS FUNÇÕES DECLARADAS!!! 
+//    -> NÃO MODIFIQUE OS PARÂMETROS DAS FUNÇÕES!!! ()
+
+
+// EXERCÍCIO 01
+function retornaTamanhoArray(array) {
+    return array.length
+   
+}
+
+// EXERCÍCIO 02
+function retornaArrayInvertido(array) {
+    return array.reverse()
+  
+}
+
+// EXERCÍCIO 03
+function retornaArrayOrdenado(array) {
+    const arrayOrdenado = array.sort((a,b)=>{
+        return a-b
+    })
+
+    return arrayOrdenado
+}
+
+// EXERCÍCIO 04
+function retornaNumerosPares(array) {
+    const arrayPares = array.filter(indice => indice % 2===0)
+    return arrayPares
+  
+}
+
+// EXERCÍCIO 05
+function retornaNumerosParesElevadosADois(array) {
+    let arrayPares = []
+
+    for (i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+          arrayPares.push(array[i]**2)
+        }
+      }
+    return arrayPares
+ 
+}
+
+// EXERCÍCIO 06
+function retornaMaiorNumero(array) {
+    return Math.max.apply( Math, array );
+  
+}
+
+// EXERCÍCIO 07
+function retornaObjetoEntreDoisNumeros(num1, num2) {
+
+}
+
+// EXERCÍCIO 08
+function retornaNPrimeirosPares(n) {
+   
+}
+
+// EXERCÍCIO 09
+function classificaTriangulo(ladoA, ladoB, ladoC) {
+
+}
+
+// EXERCÍCIO 10
+function retornaSegundoMaiorESegundoMenor(array) {
+  
+}
+
+// EXERCÍCIO 11
+function retornaChamadaDeFilme(filme) {
+   
+}
+
+// EXERCÍCIO 12
+function retornaPessoaAnonimizada(pessoa) {
+   
+}
+
+// EXERCÍCIO 13A
+function retornaPessoasAutorizadas(pessoas) {
+   
+}
+
+// EXERCÍCIO 13B
+function retornaPessoasNaoAutorizadas(pessoas) {
+  
+}
+
+// EXERCÍCIO 14
+function retornaContasComSaldoAtualizado(contas) {
+
+}
+
+// EXERCÍCIO 15A
+function retornaArrayOrdenadoAlfabeticamente(consultas) {
+  
+}
+
+// EXERCÍCIO 15B
+function retornaArrayOrdenadoPorData(consultas) {
+   
+}
